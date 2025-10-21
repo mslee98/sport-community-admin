fab3d70dce2b9fcb09eba5ed4254e975079e0a4d
이민성 <ms.lee@okestro.com>
2025-10-21 16:45:32 +0900

    fix(import): Header 컴포넌트 import 경로 수정
    - ThemeToggleButton import에 .tsx 확장자 추가
    - 모듈 해석 일관성 개선

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { ThemeToggleButton } from "../common/ThemeToggleButton";
+import { ThemeToggleButton } from "../common/ThemeToggleButton.tsx";
 import NotificationDropdown from "./NotificationDropdown";
 import UserDropdown from "./UserDropdown";
 import { Link } from "react-router";
